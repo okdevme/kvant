da5efbb0db6d4eb34ee64eedfb654ef1bf4168e8
Oleg Kapranov <presentoleg@gmail.com>
2026-08-01 21:25:14 +0300

    feat(schema): preprocess

---
+++ src/schema/preprocess.ts
@@ -0,0 +1,27 @@
+import type { KvantGenericSchema } from '../types/schema'
+import type { rawInput } from './core'
+import type { KvantPipe } from './pipe'
+import type { KvantTransform, KvantTransformDef } from './transform'
+import { pipe } from './pipe'
+import { transform } from './transform'
+
+export interface KvantPreprocess<
+  S extends KvantGenericSchema,
+  Input = unknown,
+> extends KvantPipe<
+    KvantTransform<Input, rawInput<S>>,
+    S
+  > {}
+
+export function preprocess<
+  S extends KvantGenericSchema,
+  Input = unknown,
+>(
+  def: KvantTransformDef<Input, rawInput<S>>,
+  schema: S,
+): KvantPreprocess<S, Input> {
+  return pipe(
+    transform(def),
+    schema,
+  )
+}
